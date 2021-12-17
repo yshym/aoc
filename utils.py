d01c4801d0ef4905695e376792cea1caa80913ae
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-17 22:44:25 +0200

    utils: refactor

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import sys
 from inspect import getmembers, isfunction
-from typing import Callable, List, Union
+from typing import Callable, List
 
 
 def read_str(s: str) -> List[str]:
@@ -8,7 +8,7 @@ def read_str(s: str) -> List[str]:
 
 
 def read_file(n: str) -> List[str]:
-    with open(n, "r") as f:
+    with open(n) as f:
         s = f.read()
     return read_str(s)
 
@@ -17,7 +17,7 @@ def map_strs(f: Callable, strs: List[str]):
     return list(map(f, strs))
 
 
-def run_tests(module_name):
+def run_tests(module_name: str):
     module = sys.modules[module_name]
     functions = getmembers(module, isfunction)
     for n, f in functions:
